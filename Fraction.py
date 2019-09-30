5a884d5e602b7aeab1ac17df1af5a60752c753a2
Nathan Lane <32247124+Valryn@users.noreply.github.com>
2019-09-29 21:16:39 -0700

    Add files via upload

--- Fraction.py
+++ Fraction.py
@@ -1,106 +1,147 @@
-def gcd(m, n):
-    while m % n != 0:
-        oldm = m
-        oldn = n
-
-        m = oldn
-        n = oldm % oldn
-    return n
-
-
 class Fraction:
+    """
+    A simple fraction class
+
+    Attributes:
+        numerator(int)
+        denominator(int)
+    """
 
     def __init__(self, top, bottom):
-        # Reduce inputs
-        common = gcd(top, bottom)
-
-        self.numerator = top // common
-        self.denominator = bottom // common
+        """Constructor checks for integers and auto-reduces"""
+        # Check inputs are int
+        if isinstance(top, int) and isinstance(bottom, int):
+            # Reduce inputs
+            common = Fraction.gcd(top, bottom)
+            self.numerator = top // common
+            self.denominator = bottom // common
+        else:
+            raise TypeError("Numerator and Denominator must be integers.")
 
     @property
     def numerator(self):
+        """Get and set the numerator"""
         return self.__numerator
 
     @numerator.setter
-    def numerator(self, input):
-        # Check for integer.
-        if type(input) == int:
-            self.__numerator = input
-        else:
-            raise TypeError("Numerator must be an integer.")
+    def numerator(self, newnumerator):
+        self.__numerator = newnumerator
 
     @property
     def denominator(self):
+        """Get and set the denominator, checking for 0 and correcting negative inputs"""
         return self.__denominator
 
     @denominator.setter
-    def denominator(self, input):
-        # Check for integer
-        if type(input) == int:
-            # Check for 0 or negative
-            if input > 0:
-                self.__denominator = input
-            elif input < 0:
-                # Correct for negative denominator
-                self.numerator = self.numerator * -1
-                self.denominator = self.denominator * -1
-            else:
-                raise ValueError("Denominator cannot be 0.")
+    def denominator(self, newdenominator):
+        # Check for 0 or negative
+        if newdenominator > 0:
+            self.__denominator = newdenominator
+        elif newdenominator < 0:
+            # Correct for negative denominator
+            self.numerator = self.numerator * -1
+            self.denominator = newdenominator * -1
         else:
-            raise TypeError("Denominator must be an integer.")
+            raise ValueError("Denominator cannot be 0.")
+
+    @staticmethod
+    def reciprocal(fraction):
+        """Returns the reciprocal of a fraction"""
+        newnumerator = fraction.denominator
+        newdenominator = fraction.numerator
+
+        return Fraction(newnumerator, newdenominator)
+
+    @staticmethod
+    def gcd(m, n):
+        """Returns the greatest common denominator"""
+        while m % n != 0:
+            oldm = m
+            oldn = n
+
+            m = oldn
+            n = oldm % oldn
+        return n
 
     def __str__(self):
+        """Returns object as a string"""
         return str(self.numerator) + "/" + str(self.denominator)
 
-    def show(self):
-        print(self.numerator, "/", self.denominator)
-
     def __add__(self, otherfraction):
+        """Defines + operator behavior"""
         newnumerator = self.numerator * otherfraction.denominator + \
                        self.denominator * otherfraction.numerator
         newdenominator = self.denominator * otherfraction.denominator
         return Fraction(newnumerator, newdenominator)
 
     def __eq__(self, other):
+        """Defines == operator behavior"""
         firstnumerator = self.numerator * other.denominator
         secondnumerator = other.numerator * self.denominator
 
         return firstnumerator == secondnumerator
 
     def __sub__(self, otherfraction):
-        return self
+        """Defines - operator behavior"""
+        newnumerator = self.numerator * otherfraction.denominator - \
+                       self.denominator * otherfraction.numerator
+        newdenominator = self.denominator * otherfraction.denominator
+        return Fraction(newnumerator, newdenominator)
 
     def __mul__(self, otherfraction):
-        return self
+        """Defines * operator behavior"""
+        newnumerator = self.numerator * otherfraction.numerator
+        newdenominator = self.denominator * otherfraction.denominator
+        return Fraction(newnumerator, newdenominator)
 
     def __truediv__(self, otherfraction):
-        return self
+        """Defines / operator behavior"""
+        return self * Fraction.reciprocal(otherfraction)
 
     def __gt__(self, otherfraction):
-        return self
+        """Defines > operator behavior"""
+        first = self.numerator * otherfraction.denominator
+        second = otherfraction.numerator * self.denominator
+        return first > second
 
     def __ge__(self, otherfraction):
-        return self
+        """Defines >= operator behavior"""
+        first = self.numerator * otherfraction.denominator
+        second = otherfraction.numerator * self.denominator
+        return first >= second
 
     def __lt__(self, otherfraction):
-        return self
+        """Defines < operator behavior"""
+        first = self.numerator * otherfraction.denominator
+        second = otherfraction.numerator * self.denominator
+        return first < second
 
     def __le__(self, otherfraction):
-        return self
+        """Defines <= operator behavior"""
+        first = self.numerator * otherfraction.denominator
+        second = otherfraction.numerator * self.denominator
+        return first <= second
 
     def __ne__(self, otherfraction):
-        return self
+        """Defines != operator behavior"""
+        first = self.numerator * otherfraction.denominator
+        second = otherfraction.numerator * self.denominator
+        return first != second
 
-    def __radd__(self, otherfraction):
-        return self
+    def __radd__(self, other):
+        """Defines + operator behavior when added to other objects
+        Fractions can only be added to ints"""
+        if type(other) != int:
+            raise TypeError()
+        else:
+            newfraction = Fraction(other, 1)
+        return self + newfraction
 
-    def __iadd__(self, otherfration):
-        return self
+    def __iadd__(self, otherfraction):
+        """Allows Fractions to use the += operator
+        Only fractions and ints can be added"""
+        return self + otherfraction
 
     def __repr__(self):
-        return self
-
-x = Fraction(1, 2)
-y = Fraction(2, 3)
-print(x + y)
-print(x == y)
+        """Returns identification"""
+        return "Fraction(" + str(self.numerator) + ", " + str(self.denominator) + ")"
--- FractionTests.py
+++ FractionTests.py
@@ -1,62 +1,148 @@
 import unittest
 import Fraction
 
+
 class FractionTests(unittest.TestCase):
 
     def setUp(self):
-        fraction_x = Fraction(1, 2)
-        fraction_y = Fraction(3, 4)
+        self.fraction_x = Fraction.Fraction(1, 2)
+        self.fraction_y = Fraction.Fraction(3, 4)
 
     def test_gcd(self):
-        self.assertEqual(True, False)
+        """Returns true if known GCD outcomes are correct"""
+        first_case = Fraction.Fraction.gcd(1, 2)
+        second_case = Fraction.Fraction.gcd(6, 4)
+        third_case = Fraction.Fraction.gcd(578, 9248)
+
+        self.assertEqual(first_case, 1)
+        self.assertEqual(second_case, 2)
+        self.assertEqual(third_case, 578)
 
     def test_numerator_setter(self):
-        self.assertEqual(True, False)
+        """Returns true if numerator is set correctly"""
+        self.fraction_x.numerator = 10
+        self.assertEqual(self.fraction_x.numerator, 10)
+
+        self.fraction_x.numerator = 7499291824
+        self.assertEqual(self.fraction_x.numerator, 7499291824)
 
     def test_denominator_setter(self):
-        self.assertEqual(True, False)
+        """Returns true if denominator is set correctly, 0 is accounted for, and negatives are corrected"""
+        # Check valid denominator
+        self.fraction_x.denominator = 10
+        self.assertEqual(self.fraction_x.denominator, 10)
 
-    def test_show(self):
-        self.assertEqual(True, False)
+        # Check 0
+        try:
+            self.fraction_x.denominator = 0
+        except ValueError:
+            self.assertRaises(ValueError)
+
+        # Check negative correction with positive numerator
+        self.fraction_x.denominator = -10
+        self.assertEqual(self.fraction_x.denominator, 10) and self.fraction_x.numerator < 0
+
+        # Check negative correction with negative numerator
+        self.fraction_x.numerator = -1
+        self.fraction_x.denominator = -10
+        self.assertEqual(self.fraction_x.denominator, 10) and self.fraction_x.numerator < 0
 
     def test_add(self):
-        self.assertEqual(True, False)
+        """Returns true if the "+" operator can be used, and the addition is correct."""
+        # Test known outcome (1/2) + (3/4) = (5/4)
+        result = self.fraction_x + self.fraction_y
+        self.assertEqual(result.numerator, 5)
+        self.assertEqual(result.denominator, 4)
 
     def test_eq(self):
-        self.assertEqual(True, False)
+        """Returns true if the "=" operator can be used and the numerators and denominators are equal"""
+        first = Fraction.Fraction(1, 2)
+        second = Fraction.Fraction(1, 2)
+
+        self.assertEqual(first, second)
+        self.assertEqual(first.numerator, second.numerator)
+        self.assertEqual(first.denominator, second.denominator)
 
     def test_sub(self):
-        self.assertEqual(True, False)
+        """Returns true if the "-" operator can be used, and the subtraction is correct"""
+        # Test known outcome (1/2) - (3/4) = (-1/4)
+        result = self.fraction_x - self.fraction_y
+        self.assertEqual(result.numerator, -1)
+        self.assertEqual(result.denominator, 4)
 
     def test_mul(self):
-        self.assertEqual(True, False)
+        """Returns true if the "*" operator can be used and the multiplication is correct"""
+        # Test known outcome (1/2) * (3/4) = (3/8)
+        result = self.fraction_x * self.fraction_y
+        self.assertEqual(result.numerator, 3)
+        self.assertEqual(result.denominator, 8)
 
     def test_truediv(self):
-        self.assertEqual(True, False)
+        """Returns true if the "/" operator can be used and the division is correct"""
+        # Test known outcome (1/2) / (3/4) = (2/3)
+        result = self.fraction_x / self.fraction_y
+        self.assertEqual(result.numerator, 2)
+        self.assertEqual(result.denominator, 3)
 
     def test_gt(self):
-        self.assertEqual(True, False)
+        """Returns true if the ">" operator can be used and the comparison is correct"""
+        # Test affirmative
+        self.assertTrue(self.fraction_y > self.fraction_x)
+        # Test negative
+        self.assertTrue(not self.fraction_x > self.fraction_y)
 
     def test_ge(self):
-        self.assertEqual(True, False)
+        """Returns true if the ">=" operator can be used and the comparison is correct"""
+        # Test affirmative
+        self.assertTrue(self.fraction_y >= self.fraction_x)
+        # Test negative
+        self.assertTrue(not self.fraction_x >= self.fraction_y)
+        # Test equal
+        new = Fraction.Fraction(1, 2)
+        self.assertTrue(self.fraction_x >= new)
 
     def test_lt(self):
-        self.assertEqual(True, False)
+        """Returns true if the "<" operator can be used, and the comparison is correct"""
+        # Test affirmative
+        self.assertTrue(self.fraction_x < self.fraction_y)
+        # Test negative
+        self.assertTrue(not self.fraction_y < self.fraction_x)
 
     def test_le(self):
-        self.assertEqual(True, False)
+        """Returns true if the "<=" operator can be used, and the comparison is correct"""
+        # Test affirmative
+        self.assertTrue(self.fraction_x <= self.fraction_y)
+        # Test negative
+        self.assertTrue(not self.fraction_y <= self.fraction_x)
+        # Test equal
+        new = Fraction.Fraction(1, 2)
+        self.assertTrue(self.fraction_x <= new)
 
     def test_ne(self):
-        self.assertEqual(True, False)
+        """Returns true if the "!=" comparator can be used and the numerators and denominators are unequal"""
+        self.assertNotEqual(self.fraction_x.numerator, self.fraction_y.numerator)
+        self.assertNotEqual(self.fraction_x.denominator, self.fraction_y.denominator)
 
     def test_radd(self):
-        self.assertEqual(True, False)
+        """Returns true if a fraction can be added to an integer and return a fraction"""
+        correct = Fraction.Fraction(7, 2)
+        self.assertEqual(3 + self.fraction_x, correct)
 
     def test_iadd(self):
-        self.assertEqual(True, False)
+        """Returns true if a fraction or int can be added with the "+=" operator"""
+        # Test adding a fraction
+        self.fraction_x += self.fraction_x
+        correct = Fraction.Fraction(1, 1)
+        self.assertEqual(self.fraction_x, correct)
+
+        # Test adding an int
+        self.fraction_y += 3
+        correct = Fraction.Fraction(15, 4)
+        self.assertEqual(self.fraction_y, correct)
 
     def test_repr(self):
-        self.assertEqual(True, False)
+        """Returns true if __repr__() returns a string"""
+        self.assertTrue(isinstance(self.fraction_x.__repr__(), str))
 
 
 if __name__ == '__main__':
